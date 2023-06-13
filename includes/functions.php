3a70727b78c17ca7c3ca467c38adb7929a836352
Miguel Amigot <miguel@ibleducation.com>
2023-06-13 17:32:20 -0400

    Update functions.php
    Signed-off-by: Miguel Amigot <miguel@ibleducation.com>

--- includes/functions.php
+++ includes/functions.php
@@ -28,3 +28,17 @@ function oidcg_get_authentication_url() {
 function oidcg_refresh_user_claim( $user, $token_response ) {
 	return \OpenID_Connect_Generic::instance()->client_wrapper->refresh_user_claim( $user, $token_response );
 }
+
+function redirect_sub_to_home($redirect_to_calculated, $redirect_url_specified, $user) {
+    /* If no redirect was specified, make it the homepage */
+    if (empty($redirect_to_calculated))
+        $redirect_to_calculated = get_option('siteurl');
+
+    /* If the user is not site admin, redirect to homepage */
+    if (!current_user_can('manage_options'))
+        $redirect_to_calculated = get_option('siteurl');
+
+    return $redirect_to_calculated;
+}
+
+add_filter('login_redirect', 'redirect_sub_to_home', 10, 3);
